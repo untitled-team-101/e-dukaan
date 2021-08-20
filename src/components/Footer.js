65d0c8f70cbba1462a5117c99d4078381259e660
BLiTz <69067256+PiyushNadoda@users.noreply.github.com>
2021-08-21 02:29:50 +0530

    Navbar Bana diya

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function Footer(props) {
   return (
-    <div>footer</div>
+    <div><nav class="navbar navbar-dark bg-primary">
+    <div class="container-fluid">
+      <a class="navbar-brand m-auto" rel="noreferrer" target="_blank" href="https://github.com/untitled-team-101">&copy; UNTITLED TEAM</a>
+    </div>
+  </nav>
+  </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import './styles/navbar.scss'
 
 
 function NavBar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar sticky-top navbar-expand-md navbar-dark bg-primary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#"><i class="fab fa-opencart"></i> E-DUKAAN</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"/>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
+          <div className="navbar-nav me-auto mb-2 mb-lg-0">
+            {/* <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
             <li className="nav-item">
@@ -35,11 +36,13 @@ function NavBar(props) {
             </li>
             <li className="nav-item">
               <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
-          <form className="d-flex">
+            </li> */}
+          </div>
+          <form className="d-flex ">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success" type="submit">Search</button>
+              <button className="btn btn-warning" type="submit"><i class="fal fa-search"></i></button>
+              <a className="nav-link login-btn" aria-current="page" href="#"><i class="far fa-user"></i> <span class="login-500">Login</span></a>  
+              <a className="nav-link cart-btn" aria-current="page" href="#"><i class="fad fa-shopping-cart"></i></a>  
           </form>
         </div>
       </div>
