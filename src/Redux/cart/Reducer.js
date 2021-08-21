b5af9372a826bd82ec7a8a3a9177312568c12a46
Ayan Dhara <37933140+Ayan-Dhara@users.noreply.github.com>
2021-08-21 23:29:36 +0530

    fixed cart multiple add to single time add

--- src/Redux/cart/Reducer.js
+++ src/Redux/cart/Reducer.js
@@ -5,11 +5,13 @@ const initialState = JSON.parse(localStorage.getItem("cart")) || []
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART:
+      if (state.filter(product => product.id === action.payload.id).length > 0)
+        return state
       state = [...state, action.payload]
       localStorage.setItem("cart", JSON.stringify(state))
       return state
     case REMOVE_FROM_CART:
-      state =  [...state.filter(product => product.id !== action.payload.id)]
+      state = [...state.filter(product => product.id !== action.payload.id)]
       localStorage.setItem("cart", JSON.stringify(state))
       return state
     default:
