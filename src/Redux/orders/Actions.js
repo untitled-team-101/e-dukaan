475fe70765e15be316abecaf671821ccc367b6c6
Ayan Dhara <37933140+Ayan-Dhara@users.noreply.github.com>
2021-08-27 01:58:48 +0530

    page build

--- src/Redux/orders/Actions.js
+++
@@ -1,8 +0,0 @@
-import {ADD_ORDER} from "../CONSTANTS";
-
-export const addOrder = (order) => {
-  return {
-    type: ADD_ORDER,
-    payload: order
-  }
-}
--- src/components/Register.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Register(props) {
-  return (
-    <div>register</div>
-  );
-}
-
-export default Register;
